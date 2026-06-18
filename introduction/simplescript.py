cbd1065471845ec5494d150287aa2f45e119caa0
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-18 13:57:55 +0300

    Added comments on code

--- introduction/simplescript.py
+++ introduction/simplescript.py
@@ -4,7 +4,7 @@ import cv2 # importing the library
 import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
-img = cv2.imread(os.path.join(base_dir, "../assets/setup.jpg"), -1)
+img = cv2.imread(os.path.join(base_dir, "../assets/setup.jpg"), 0)
 
 """
 cv2.IMREAD_COLOR / 1 : Loads a color image. Any transparency of image will be neglected. It is the default flag.
@@ -13,5 +13,5 @@ cv2.IMREAD_UNCHANGED / -1 : Loads image as such including alpha channel
 """
 
 cv2.imshow("Image", img ) # displaying the image
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.waitKey(0) # waits a specified amount of time before executing the next line of code, 0 is infinite
+cv2.destroyAllWindows() # destroys the window when any key is pressed
