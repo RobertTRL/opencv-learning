ccbced8520784ce1a1888c0fa8da346817eb6222
RobertTRL <robertojnr2802@gmail.com>
2026-06-19 10:53:05 +0300

    Added code on splitting window into 4 sections, and displaying the frame on each section

--- videocapture/script.py
+++ videocapture/script.py
@@ -5,7 +5,19 @@ capture = opencv.VideoCapture(1) # Enter a filename or a number corresponding to
 
 while True: # Loops indefinitely
     ret, frame = capture.read()
-    opencv.imshow("Frame", frame)
+
+    width = int(capture.get(3))
+    height = int(capture.get(4))
+
+    image = np.zeros(frame.shape, np.uint8)
+    smaller_frame = opencv.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    image[:height // 2, :width // 2] = smaller_frame # top-left
+    image[height // 2:, :width // 2] = smaller_frame # bottom-left
+    image[:height // 2, width // 2:] = smaller_frame # top-right
+    image[height // 2:, width // 2:] = smaller_frame # bottom-right
+
+    opencv.imshow("Frame", image)
 
     if opencv.waitKey(1) == ord('q'):
         break
