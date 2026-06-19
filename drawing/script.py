fbc5f0c28c2ad86d9e3b5d13c91464b6fd20c669
RobertTRL <robertojnr2802@gmail.com>
2026-06-19 12:35:13 +0300

    Added foont function

--- drawing/script.py
+++ drawing/script.py
@@ -10,6 +10,9 @@ while True:
 
     image = opencv.line(frame, (width // 2, ((height // 2) - 10)), (width // 2, ((height // 2) + 10)), (0, 0, 255), 2)
     image = opencv.line(image, (((width // 2) - 10), height // 2), (((width // 2) + 10), height // 2), (0, 0, 255), 2)
+    image = opencv.rectangle(image, (width // 2, height // 2), (width, height), (255, 0, 0), 1)
+    image = opencv.circle(image, (300,300), 100, (0, 255, 0, 0.3 ), -1)
+    image = opencv.putText(image, "Robert is Great!", (200, height - 10), opencv.FONT_HERSHEY_SIMPLEX, 1 ,(255, 255, 255), 5, opencv.LINE_AA)
 
     opencv.imshow("Frame", image)
 
