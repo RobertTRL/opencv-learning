29307109574f0c1f8b63a96a698986a5b9ee55da
RobertTRL <robertojnr2802@gmail.com>
2026-06-19 12:10:11 +0300

    Added code to draw a crosshair on the frame

--- drawing/script.py
+++ drawing/script.py
@@ -1,6 +1,6 @@
 import cv2 as opencv
 
-capture = opencv.VideoCapture(0)
+capture = opencv.VideoCapture(1)
 
 while True:
     ret, frame = capture.read()
@@ -8,8 +8,8 @@ while True:
     width = int(capture.get(3))
     height = int(capture.get(4))
 
-    image = opencv.line(frame, (0, 0), (width, height), (0, 0, 255), 6)
-    image = opencv.line(image, (0, height), (width, 0), (0, 0, 255), 6)
+    image = opencv.line(frame, (width // 2, ((height // 2) - 10)), (width // 2, ((height // 2) + 10)), (0, 0, 255), 2)
+    image = opencv.line(image, (((width // 2) - 10), height // 2), (((width // 2) + 10), height // 2), (0, 0, 255), 2)
 
     opencv.imshow("Frame", image)
 
